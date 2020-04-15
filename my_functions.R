becbfd084e84f9652664fef39e5d0d995da54d5d
Konstantinos Stefanopoulos <costisst@hotmail.com>
2020-04-15 14:47:24 +0300

    Add files via upload

--- my_functions.R
+++ my_functions.R
@@ -1,4 +1,4 @@
-
+library(hunspell)
 ############################################################################################################################
 ############################################################################################################################
 ############################################################################################################################
@@ -68,3 +68,80 @@ clear_tweets_df <- function(tweets.df)
   return(tweets.df)
 }
 
+############################################################################################################################
+############################################################################################################################
+############################################################################################################################
+
+#function to perform basic tweet cleaning
+remove_slang_misspells <- function(tweets_movie_clean)
+{
+  # Collect all slang/misspelled words
+  #get all tweets as one string
+  oneTweet<-toString(tweets_movie_clean)
+  # get list of all words
+  words <- strsplit(oneTweet, " ")[[1]]
+  
+  badWords<-hunspell(words)
+  badWords<-badWords[lapply(badWords,length)>0]
+  badWords<-tolower(badWords)
+  
+  #split into groups as too large to remove all at once
+  group <- 100
+  n <- length(badWords)
+  r <- rep(1:ceiling(n/group),each=group)[1:n]
+  d <- split(badWords,r)
+  
+  # Remove "bad words" and slang
+  for (i in 1:length(d)) {
+    
+    tweets_movie_clean <- removeWords(tweets_movie_clean, c(paste(d[[i]])))
+    
+  }
+  return(tweets_movie_clean)
+}
+
+############################################################################################################################
+############################################################################################################################
+############################################################################################################################
+
+# Cleaning procedure
+cleanse_them_tweets <- function(tweets.df)
+{
+  # Remove the tweet text URLs
+  tweets_movie_clean<- clear_tweets_df(tweets.df)
+
+  # Remove special char,punctuations & number
+  tweets_movie_clean<-gsub("[^A-Za-z]"," ",tweets_movie_clean)
+
+  # LowerCase
+  tweets_movie_clean <- tolower(tweets_movie_clean)
+
+  # Remove punctuations
+  tweets_movie_clean <- removePunctuation(tweets_movie_clean)
+
+  # Remove stopwords
+  tweets_movie_clean <- removeWords(tweets_movie_clean, stopwords("english"))
+  
+  # Removing custom stop words
+  custom_stop <- c("movie","parasite","s","t","m","y","re","get","re",
+                   "also","can","amp","one","like","will","go",
+                   "know","us","just","now","ve","got","haven",
+                   "won","don","ho","get","say","even","didn",
+                   "oscars","oscar","nominees","academyawards",
+                   "theoscars","awards","movie", "night", "film",
+                   "carpet", "red","actor","actress")
+  tweets_movie_clean <- removeWords(tweets_movie_clean, custom_stop)
+  
+  # Remove unecessary spaces
+  tweets_movie_clean <- stripWhitespace(tweets_movie_clean)
+
+  
+  # Remove duplicates
+  tweets_movie_clean <- unique(tweets_movie_clean)
+
+  # Remove slang/misspells
+  tweets_movie_clean <- remove_slang_misspells(tweets_movie_clean)
+  
+  return(tweets_movie_clean)
+
+}
